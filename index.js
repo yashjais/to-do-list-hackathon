a5df4c0505e8a23d61ae06df9312665debf2fc5d
Yash Jaiswal <jais.yash01@gmail.com>
2020-05-30 23:46:34 +0530

    routing

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3010
+const setUpDB = require('./config/database')
+const router = require('./config/routes')
+
+app.use('/', router)
+
+setUpDB()
 
 app.listen(port, () => {
     console.log('listening on the port', port)
